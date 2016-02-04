9b43138859cb05b0d2e4dd4ce1f183874563b04c
Donovan Hutchinson <donovan@kitmanlabs.com>
2016-02-04 08:49:22 +0000

    Updating v5 to use a cursor for the current cat

--- gulpfile.js
+++ gulpfile.js
@@ -23,11 +23,11 @@ gulp.task('styles', function() {
   gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('public/stylesheets'))
+    .pipe(gulp.dest('_build/stylesheets'))
     .pipe(rename({suffix: '.min'}))
     //.pipe(minifycss())
     //.pipe(cssshrink())
-    .pipe(gulp.dest('public/stylesheets'))
+    .pipe(gulp.dest('_build/stylesheets'))
     .pipe(browserSync.reload({stream:true}));
 });
 
@@ -37,57 +37,57 @@ gulp.task('scripts', function() {
     //.pipe(jshint.reporter('default'))
     .pipe(plumber())
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('public/javascripts'))
+    .pipe(gulp.dest('_build/javascripts'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(gulp.dest('public/javascripts'))
+    .pipe(gulp.dest('_build/javascripts'))
     .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('vendor-scripts', function() {
   return gulp.src(['./src/javascripts/vendor/*.js'])
     .pipe(plumber())
-    .pipe(gulp.dest('public/javascripts/vendor'))
+    .pipe(gulp.dest('_build/javascripts/vendor'))
     .pipe(browserSync.reload({stream:true}));
 });
 
 // Optimizes the images that exists
 gulp.task('images', function () {
   return gulp.src('src/images/**')
-    .pipe(changed('public/images'))
+    .pipe(changed('_build/images'))
     .pipe(imagemin({
       // Lossless conversion to progressive JPGs
       progressive: true,
       // Interlace GIFs for progressive rendering
       interlaced: true
     }))
-    .pipe(gulp.dest('public/images'))
+    .pipe(gulp.dest('_build/images'))
     .pipe(size({title: 'images'}));
 });
 
 gulp.task('html', function() {
   gulp.src('./src/**/*.html')
-    .pipe(gulp.dest('public/'))
+    .pipe(gulp.dest('_build/'))
 });
 
 gulp.task('browser-sync', ['styles', 'scripts'], function() {
   browserSync({
     server: {
-      baseDir: "./public/",
+      baseDir: "./_build/",
       injectChanges: true // this is new
     }
   });
 });
 
 gulp.task('deploy', function() {
-  return gulp.src('./public/**/*')
+  return gulp.src('./_build/**/*')
     .pipe(ghPages());
 });
 
 gulp.task('watch', function() {
   // Watch .html files
   gulp.watch('src/**/*.html', ['html', browserSync.reload]);
-  gulp.watch("public/*.html").on('change', browserSync.reload);
+  gulp.watch("_build/*.html").on('change', browserSync.reload);
   // Watch .sass files
   gulp.watch('src/sass/**/*.scss', ['styles', browserSync.reload]);
   // Watch .js files
